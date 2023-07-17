ffca7f3a5f9c29eed390e046b9e60a159e7e5a4d
gracebir <grace.devolop@gmail.com>
2023-07-17 20:11:25 +0200

    protect the route

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,6 +1,11 @@
+import { verifyJwt } from "@/lib/jwt";
 import prisma from "@/lib/prisma";
 
 export async function GET(request:Request, {params}: {params: {id: number}}){
+    const accessToken = request.headers.get("Authorization")
+    if(!accessToken || !verifyJwt(accessToken)){
+        return new Response(JSON.stringify({error: "unauthorized"}), {status: 401})
+    }
     const usePost = await prisma.post.findMany({
         where: {
             authorId: +params.id
